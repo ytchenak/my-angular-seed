48bf9b94dd5735269b4359ca60ec4106f2ddc568
Tchenak, Yakov <yakov_tchenak@bmc.com>
2016-07-02 20:16:20 +0300

    remove typings directory

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/angular-ui-bootstrap/index.d.ts" />
-/// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
